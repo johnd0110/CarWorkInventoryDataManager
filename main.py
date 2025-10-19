3b462e6a333db2264727fa2349253e13e641d2e3
johnd <johndiep0110@gmail.com>
2025-10-19 17:26:28 -0400

    New Repository

---
+++ main.py
@@ -0,0 +1,75 @@
+from tkinter import *
+from tkinter import ttk
+
+from datetime import datetime
+from dataclasses import dataclass
+
+class Car:
+    def __init__(self, make, model, year, engineType, mileage):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.engineType = engineType
+        self.mileage = mileage
+        self.parts = []
+        self.performedLabor = []
+
+    def addPart(self, taxes, shippingCost, fullCost):
+        part = CarPart(taxes, shippingCost, fullCost)
+        self.parts.append(part)
+        return part
+
+    def addPart(self, part):
+        self.parts.append(part)
+
+    def addEmployee(self, name, payRate):
+        employee = Employee(name, payRate)
+        self.performedLabor.append(employee)
+        return employee
+
+    def addEmployee(self, employee):
+        self.performedLabor.append(employee)
+
+    def getTotalHoursWorked(self):
+        pass
+
+    def getTotalWages(self):
+        pass
+
+@dataclass
+class CarPart:
+    taxes: float
+    shippingCost: float
+    fullCost: float
+
+@dataclass
+class Employee:
+    name: str
+    payRate: float
+
+@dataclass
+class Labor:
+    employee: Employee
+    workDate: datetime
+    # Consider changes to pay rate and prorating
+    hoursWorked: float
+    workDescription: str
+
+
+if __name__ == '__main__':
+
+    class testApp:
+        def __init__(self, tkRoot):
+            self.root = tkRoot
+            self.root.title("Test Car Inventory Manager")
+
+            self.mainframe = ttk.Frame( self.root, padding=(3,3,12,12))
+            self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+
+            
+
+
+    root = Tk()
+    testApp(root)
+    root.mainloop()
+
